9d959f7bf1b3a128116b8336ff98ec5a01f7f252
Piccolo7210 <mustakim.mohsin7210@gmail.com>
2025-06-28 16:27:14 +0600

    sads

--- server/server.js
+++ server/server.js
@@ -31,5 +31,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Rider Server running on port ${PORT}`);
 });
